acf0a3685a08f03c31490ee57c0a0789688af0c2
kotarokotarokotaro <51646963+kotarokotarokotaro@users.noreply.github.com>
2020-04-30 11:22:37 +0900

    update test1.py

--- test1.py
+++ test1.py
@@ -7,11 +7,12 @@ import numpy as np
 import sys
 
 x_min = 0 #各自調整してください
-x_max = 1060
-y_min = 60
-y_max = 760
-interval = 0.5 #画面収録の間隔です[s]
-threshold = 10.0 #画面変化検知の閾値です[%]
+x_max = 1440 - 215
+y_min = 0
+y_max = 900 - 45
+interval = 1.0 #画面収録の間隔です
+percent = 5.0
+threshold = 10.0
 
 area = (x_max - x_min)*(y_max - y_min)
 
@@ -36,9 +37,9 @@ j = 0
 try:
     while True:
         im = ImageGrab.grab(bbox=(x_min, y_min, x_max, y_max))
-        im_mat = np.array(im)
-        diff = im_mat - im_mat_past
-        diff_per = np.count_nonzero(diff > 0) / (area*3) * 100
+        im_mat = np.array(im.convert('L'))
+        diff = np.abs(im_mat - im_mat_past)
+        diff_per = (np.count_nonzero(diff > int(percent * 256.0 / 100.0)) / area ) * 100
         print(diff_per)
         if diff_per > threshold :
             im.save('captured_'+str(j)+'.png')
